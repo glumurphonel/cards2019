3c6bdecdba24570d01169cd62b84da5cba6bf3b5
Kirill Korchemkin <kir.korchemkin@gmail.com>
2019-02-02 17:45:54 +0100

    Updated GUI template.

--- app/src/App.js
+++ app/src/App.js
@@ -26,18 +26,6 @@ class App extends Component {
       this.contractOperations.readAccount(account => {
         this.setState({ account: account })
       })
-
-      this.contractOperations.ticketContract.deployed().then((instance) => {
-        instance.allEvents(function(error, log){
-        if (!error)
-          console.log(log)
-        })
-        this.contractOperations.web3.currentProvider.publicConfigStore.on('update', (data)=>{
-          if(data.selectedAddress !== this.state.account.address) {
-            window.location.reload(false)
-          }
-        })
-      })
     } catch (e) {
       alert(e)
     }
